0126eb43ac989aded34218195cc2c544a8d2bcd1
rudanok <a.rudanok@gmail.com>
2023-11-29 11:10:11 +0300

    задание 6

---
+++ work_06/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <HeaderBlock></HeaderBlock>
+    <!-- <HomePage></HomePage> -->
+    <!-- <BlogPage></BlogPage> -->
+    <!-- <ProjectPage></ProjectPage> -->
+    <ProjectsListPage></ProjectsListPage>
+    <FooterBlock></FooterBlock>
+  </div>
+</template>
+
+<script>
+import HeaderBlock from './blocks/HeaderBlock.vue'
+// import HomePage from './pages/HomePage.vue'
+// import BlogPage from './pages/BlogPage.vue'
+// import ProjectPage from './pages/ProjectPage.vue'
+import ProjectsListPage from './pages/ProjectsListPage.vue'
+import FooterBlock from './blocks/FooterBlock.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderBlock,
+    // HomePage,
+    // BlogPage,
+    // ProjectPage,
+    ProjectsListPage,
+    FooterBlock
+  }
+}
+</script>
+
+<style lang="css">
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
