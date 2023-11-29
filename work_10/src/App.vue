c14b161be95c9b432afecbd16f9ec4b4754515a0
rudanok <a.rudanok@gmail.com>
2023-11-29 13:54:42 +0300

    задание к семинару 10

---
+++ work_10/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <HeaderBlock></HeaderBlock>
+    <!-- <HomePage></HomePage> -->
+    <!-- <BlogPage></BlogPage> -->
+    <!-- <ProjectPage></ProjectPage> -->
+    <!-- <ProjectsListPage></ProjectsListPage> -->
+    <ProductDetailsPage></ProductDetailsPage>
+    <FooterBlock></FooterBlock>
+  </div>
+</template>
+
+<script>
+import HeaderBlock from './blocks/HeaderBlock.vue'
+// import HomePage from './pages/HomePage.vue'
+// import BlogPage from './pages/BlogPage.vue'
+// import ProjectPage from './pages/ProjectPage.vue'
+// import ProjectsListPage from './pages/ProjectsListPage.vue'
+import ProductDetailsPage from './pages/ProductDetailsPage.vue'
+import FooterBlock from './blocks/FooterBlock.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderBlock,
+    // HomePage,
+    // BlogPage,
+    // ProjectPage,
+    // ProjectsListPage,
+    ProductDetailsPage,
+    FooterBlock
+  }
+}
+</script>
+
+<style lang="css">
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ work_10/src/blocks/HeaderBlock.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header class="w1200">
+            <div class="logo">
+                <img src="@/assets/logo.png" alt="logo">
+                Interno
+            </div>
+
+            <nav>
+                <div class="menu">
+                    <a href="#">Home</a>
+                    <a href="#">Project</a>
+                    <a href="#">Blog</a>
+                </div>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBlock'
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
